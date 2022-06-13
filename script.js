88421ec50c8b129a4223b6bdb6ea9b266f9d9ed4
arabiu033 <arabiu033@gmail.com>
2022-06-13 06:57:31 +0000

    Completed the js file. time for debugging.

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 // creating a game :)
 
+let p=0;
+let c=0;
+
 function computerPlay(){
   let choose = Math.floor(Math.random * 100);
-  let dec1 = "Rock";
-  let dec2 = "Paper";
-  let dec3 = "Scissors"
+  let dec1 = "rock";
+  let dec2 = "paper";
+  let dec3 = "scissors"
   let pick;
 
   if (choose % 3 == 0 || choose % 5 == 0)
@@ -15,4 +18,42 @@ function computerPlay(){
     pick = dec3;
 
   return pick;
+}
+
+function playRound(playerSelection, computerSelection){
+  let winner = "";
+  let holder = playerSelection.concat(computerSelection);
+  if (holder == "rockrock" || holder == "paperpaper" || holder == "scissorsscissors")
+    winner = "It's a tie. Nobody Wins";
+  else if (holder == "rockpaper" || holder == "paperrock") {
+    winner = (playerSelection == "paper") ? "You Win! "+ playerSelection +" beats "+ computerSelection : "You Lose! "+ computerSelection +" beats "+ playerSelection;
+    playerSelection === "paper" ? ++p : ++c;
+    }
+  else if (holder == "rockscissors" || holder == "scissorsrock"){
+    winner = (playerSelection == "rock") ? "You Win! "+ playerSelection +" beats "+ computerSelection : "You Lose! "+ computerSelection +" beats "+ playerSelection;
+    playerSelection === "rock" ? ++p : ++c;
+    }
+  else if (holder == "paperscissors" || holder == "scissorspaper"){
+    winner = (playerSelection == "scissors") ? "You Win! "+ playerSelection +" beats "+ computerSelection : "You Lose! "+ computerSelection +" beats "+ playerSelection;
+    playerSelection === "scissors" ? ++p : ++c;
+  else
+    winner ="Something is wrong. check your spellings";
+
+  return winner;
+}
+
+function game(){
+  for (let i=0; i<5; i++){
+    const player = (prompt("Lets play! Enter rock, paper or scissors:").toLowerCase();
+      const computer = computerPlay();
+
+      if (player === "rock" || player === "paper" || player === "scissors"){
+	console.log(playerRound(player, computer));
+	console.log("Score board:\n"+ "You - "+ p +"\n"+ "Computer - "+ c);	
+      }
+      else {
+	console.log("Inputed a wrong value. Check your spellings");
+	--i;
+      }
+  }
 }
---
+++ script.js~
@@ -0,0 +1,18 @@
+// creating a game :)
+
+function computerPlay(){
+  let choose = Math.floor(Math.random * 100);
+  let dec1 = "Rock";
+  let dec2 = "Paper";
+  let dec3 = "Scissors"
+  let pick;
+
+  if (choose % 3 == 0 || choose % 5 == 0)
+    pick = dec1;
+  else if (choose % 2 == 0)
+    pick = dec2;
+  else
+    pick = dec3;
+
+  return pick;
+}
