4d4b0133f61476111a1855446cd374d6af9407e9
Aminu Rabiu <arabiu033@gmail.com>
2022-06-18 16:50:00 +0100

    Completed modifying the game on branch rps-ui

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // creating a game :)
 
+let score = document.querySelectorAll('li');
+let msg = document.querySelector('#msg');
 let p=0;
 let c=0;
 let round=true;
@@ -22,50 +24,54 @@ function computerPlay(){
 }
 
 function playRound(playerSelection, computerSelection){
-  let winner = "";
+
   let holder = playerSelection.concat(computerSelection);
+
   if (holder === "rockrock" || holder === "paperpaper" || holder === "scissorsscissors")
-    winner = "It's a tie. Nobody Wins";
+    msg.textContent = "It's a tie. Nobody Wins";
   else if (holder === "rockpaper" || holder === "paperrock") {
-    winner = (playerSelection === "paper") ? "You Win! "+ playerSelection +" beats "+ computerSelection : "You Lose! "+ computerSelection +" beats "+ playerSelection;
+    msg.textContent = (playerSelection === "paper") ? "You Win! "+ playerSelection +" beats "+ computerSelection : "You Lose! "+ computerSelection +" beats "+ playerSelection;
     playerSelection === "paper" ? ++p : ++c;
     }
   else if (holder === "rockscissors" || holder === "scissorsrock"){
-    winner = (playerSelection === "rock") ? "You Win! "+ playerSelection +" beats "+ computerSelection : "You Lose! "+ computerSelection +" beats "+ playerSelection;
+    msg.textContent = (playerSelection === "rock") ? "You Win! "+ playerSelection +" beats "+ computerSelection : "You Lose! "+ computerSelection +" beats "+ playerSelection;
     playerSelection === "rock" ? ++p : ++c;
     }
   else if (holder === "paperscissors" || holder === "scissorspaper"){
-    winner = (playerSelection === "scissors") ? "You Win! "+ playerSelection +" beats "+ computerSelection : "You Lose! "+ computerSelection +" beats "+ playerSelection;
+    msg.textContent = (playerSelection === "scissors") ? "You Win! "+ playerSelection +" beats "+ computerSelection : "You Lose! "+ computerSelection +" beats "+ playerSelection;
     playerSelection === "scissors" ? ++p : ++c;
     }
-  else
-    winner ="Something is wrong. check your spellings";
-
-  return winner;
+    else {
+      
+    }
 }
 
-function game(){
-  for (;round;){
-    const player = (prompt("Lets play! Enter rock, paper or scissors:").toLowerCase());
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {game(button.id)});
+});
+
+function game(btn){
+
     const computer = computerPlay();
 
-      if (player === "rock" || player === "paper" || player === "scissors"){
-	console.log(playRound(player, computer));
-	console.log("Score board:\n"+ "You - "+ p +"\n"+ "Computer - "+ c);	
+    if (btn === "rock" || btn === "paper" || btn === "scissors"){
+    	playRound(btn, computer);
+      score[1].textContent = "You - "+ p;
+      score[2].textContent = "Computer - "+ c;	
       }
-      else 
-	console.log("Inputed a wrong value. Check your spellings");
+    else 
+    	console.log("Inputed a wrong value. Check your spellings");
 
     if (c === 5 || p === 5){
-      round = false;
-      if (c === 5)
-	console.log("\t\tCOMPUTER WINS!!!");
-      else
-	console.log("\t\tYOU WINS!!!");
-    }
+      c = 0;
+      p = 0;
+      score[1].textContent = "You - "+ p;
+      score[2].textContent = "Computer - "+ c;	
+     if (c === 5) 
+        msg.textContent = "YOU LOST. TRY AGAIN"
+     else
+      	msg.textContent = "CONGRATULATIONS!!! YOU WIN,"
   }
 
-  p = 0;
-  c = 0;
-  round = true;
 }
